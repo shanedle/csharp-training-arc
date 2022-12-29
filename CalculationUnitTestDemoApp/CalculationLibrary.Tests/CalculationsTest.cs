2d32f6f8fb7f5d9fca91cff37d7315cb72991a9c
Shane <31248360+shanedle@users.noreply.github.com>
2022-12-29 16:05:56 +0100

    :sparkles: added unit tests mini project

---
+++ CalculationUnitTestDemoApp/CalculationLibrary.Tests/CalculationsTest.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+
+namespace CalculationLibrary.Tests
+{
+    public class CalculationsTest
+    {
+        [Theory]
+        [InlineData(5,5,10)]
+        public void AddShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            // arrange
+            Calculations calc = new Calculations();
+
+            // act
+            double actual = calc.Add(x, y);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(15, 5, 10)]
+        public void SubtractShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            // arrange
+            Calculations calc = new Calculations();
+
+            // act
+            double actual = calc.Subtract(x, y);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(5, 5, 25)]
+        public void MultiplyShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            // arrange
+            Calculations calc = new Calculations();
+
+            // act
+            double actual = calc.Multiply(x, y);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(10, 0, 0)]
+        public void DivideShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            // arrange
+            Calculations calc = new Calculations();
+
+            // act
+            double actual = calc.Divide(x, y);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CalculationUnitTestDemoApp/CalculationLibrary/Calculations.cs
@@ -0,0 +1,32 @@
+﻿namespace CalculationLibrary
+{
+    public class Calculations
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x,double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x,double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x,double y)
+        {
+            double output = 0;
+
+            if(y != 0)
+            {
+                output = x / y;
+            }
+
+            return output;
+        }
+    }
+}
