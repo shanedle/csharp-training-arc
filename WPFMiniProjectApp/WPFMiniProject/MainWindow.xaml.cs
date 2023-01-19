2fb0c7754b70ca941062518a533f7e25de2d94d3
Shane <31248360+shanedle@users.noreply.github.com>
2023-01-19 01:04:44 +0100

    :sparkles: added wpf mini project

---
+++ WPFMiniProjectApp/WPFMiniProject/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMiniProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ISaveAddress
+    {
+        BindingList<AddressModel> addresses = new BindingList<AddressModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            addressesList.ItemsSource = addresses;
+        }
+
+        public void SaveAddress(AddressModel address)
+        {
+            addresses.Add(address);
+        }
+
+        private void AddAddress_Click(object sender, RoutedEventArgs e)
+        {
+            AddressEntry entry = new AddressEntry(this);
+
+            entry.Show();
+        }
+
+        private void SavePerson_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = firstNameText.Text,
+                LastName = lastNameText.Text,
+                IsActive = (activeCheckbox.IsChecked ?? false),
+                Addresses = addresses.ToList()
+            };
+        }
+    }
+}
