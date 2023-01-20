d6c19da03efb0dfd3235538d842dc81bce9e2303
Shane <31248360+shanedle@users.noreply.github.com>
2023-01-20 15:59:16 +0100

    :sparkles: added api mini project

---
+++ ApiMiniProjectApp/ApiMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,25 @@
+﻿using ApiMiniProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMiniProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST api/<AddressController>
+        [HttpPost]
+        public void Post([FromBody] AddressModel data)
+        {
+            _logger.LogInformation("The address was logged as {Address}", data);
+        }
+    }
+}
